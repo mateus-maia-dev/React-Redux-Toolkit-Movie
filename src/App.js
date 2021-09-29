32d16c155478738ba5d048f18c3e43a0a5aed0f7
mateusmaiaf <eng.fonsecamateus@gmail.com>
2021-09-29 20:01:19 -0300

    feat: structuring folders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
 
-import './App.css';
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      App
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/movie/:imdbID" component={MovieDetail} />
+          <Route component={PageNotFound} />
+        </Switch>
+        <Route component={Footer} />
+      </Router>
     </div>
   );
 }
